4219ca6a0f61f933b17806b582341acb6e5a2a79
daniel <mayowaaloko@gmail.com>
2025-10-23 14:12:38 +0100

    ETL pipeline completed

--- test_mongodb.py
+++ test_mongodb.py
@@ -1,7 +1,7 @@
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
-uri = "mongodb+srv://mayowaaloko_db_user:danielshola001@cluster0.eephwie.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+uri = "mongodb+srv://mayowaaloko_db_user:<password>@cluster0.eephwie.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 # Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi("1"))
