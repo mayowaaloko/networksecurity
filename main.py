afe44d1de5508f663c151ba5e1ea64ff26874006
daniel <mayowaaloko@gmail.com>
2025-10-24 15:34:15 +0100

    Data transformation initiated

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from networksecurity.components.data_ingestion import DataIngestion
+from networksecurity.components.data_transformation import DataTransformation
 from networksecurity.exception.exception import NetworkException
 from networksecurity.logging.logger import logging
 from networksecurity.components.data_validation import DataValidation
-from networksecurity.entity.config_entity import DataIngestionConfig, DataValidationConfig
+from networksecurity.entity.config_entity import DataIngestionConfig, DataValidationConfig, DataTransformationConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
 import sys
 
@@ -26,6 +27,13 @@ if __name__ == "__main__":
             data_validation_artifact = data_validation.initiate_data_validation()
             logging.info("completed the data validation")
             print(data_validation_artifact)
+            
+            data_transformation_config = DataTransformationConfig(trainingpipeline_config)
+            data_transformation = DataTransformation(data_validation_artifact, data_transformation_config)
+            logging.info("initiate the data transformation")
+            data_transformation_artifact = data_transformation.initiate_data_transformation()
+            logging.info("completed the data transformation")
+            print(data_transformation_artifact)
 
             
         except Exception as e:
--- networksecurity/constant/training_pipeline/__init__.py
+++ networksecurity/constant/training_pipeline/__init__.py
@@ -6,15 +6,15 @@ import numpy as np
 """
 defining common constant variable for training pipeline
 """
-TARGET_COLUMN = 'Result'
-PIPELINE_NAME: str = 'NetworkSecurity'
-ARTIFACT_DIR: str = 'artifacts'
-FILE_NAME: str = 'phisingData.csv'
+TARGET_COLUMN = "Result"
+PIPELINE_NAME: str = "NetworkSecurity"
+ARTIFACT_DIR: str = "artifacts"
+FILE_NAME: str = "phisingData.csv"
 
-TRAIN_FILE_NAME: str = 'train.csv'
-TEST_FILE_NAME: str = 'test.csv'
+TRAIN_FILE_NAME: str = "train.csv"
+TEST_FILE_NAME: str = "test.csv"
 
-SCHEMA_FILE_PATH: str = os.path.join('data_schema', 'schema.yaml')
+SCHEMA_FILE_PATH: str = os.path.join("data_schema", "schema.yaml")
 
 
 """
@@ -35,3 +35,20 @@ DATA_VALIDATION_VALID_DIR: str = "validated"
 DATA_VALIDATION_INVALID_DIR: str = "invalid"
 DATA_VALIDATION_DRIFT_REPORT_DIR: str = "drift_report"
 DATA_VALIDATION_DRIFT_REPORT_FILE_NAME: str = "drift_report.yaml"
+PREPROCESSING_OBJECT_FILE_NAME: str = "preprocessing.pkl"
+
+
+"""
+Data Transformation related content start with DATA_TRANSFORMATION_
+"""
+
+DATA_TRANSFORMATION_DIR_NAME: str = "data_transformation"
+DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR: str = "transformed"
+DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR: str = "transformed_object"
+
+# knn imputer
+DATA_TRANSFORMATION_IMPUTER_PARAMS: dict = {
+    "missing_values": np.nan,
+    "n_neighbors": 3,
+    "weights": "uniform",
+}
